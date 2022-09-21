e4640abe10a596cbb0a5907aca28a16863219eca
Hannah <hdwells@crimson.ua.edu>
2022-09-20 21:47:35 -0500

    finished

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var len;
 var results = '';
 
+function search() { 
+    apiSearch();
+    document.getElementById("query").value = " ";
+}
+
 function apiSearch() {
   var params = {
     "q": $("#query").val(),
     "count": "50",
     "offset": "0", 
     "mkt": "en-us"
-  };
+    };
 
   $.ajax({
       url: 'https://api.bing.microsoft.com/' + "/v7.0/search?" +$.param(params),
@@ -17,7 +22,8 @@ function apiSearch() {
       type: "GET",
     })
     .done(function (data) {
-      len = data.webPages.value.length;
+        len = data.webPages.value.length;
+        results = "";
       for (i = 0; i < len; i++) {
         results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
       }
@@ -30,3 +36,67 @@ function apiSearch() {
     });
 }
 
+
+function bkgImage() {
+    document.body.style.backgroundImage = "url('https://freight.cargo.site/t/original/i/561b7e4736035c98687372ba6c41ef46377c916705ccecd0498a526089aa8634/_DSF0752-Exposure.jpg')";
+}
+
+
+
+function Time() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let session = "AM";
+
+    if (hh == 0) {
+        hh = 12;
+    }
+    if (hh > 12) {
+        hh = hh - 12;
+        session = "PM";
+    }
+
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+   
+
+    let time = hh + ":" + mm + "" + session;
+    $("#time").css("visibility", "visible");
+    $('#time').html(time);
+    $('#time').dialog();
+}
+
+
+
+function feelingLucky() {
+  var params = {
+    "q": $("#query").val(),
+    "count": "50",
+    "offset": "0", 
+    "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/' + "/v7.0/search?" + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "30836d683a4a4e3f84cacb756aa1168b");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            results = '';
+            for (i = 0; i < 1; i++) {
+                window.location.href = data.webPages.value[i].url
+                }
+            })
+    .fail(function () {
+      alert("error");
+    });
+}
+
+
+function bkgImage() {
+    document.body.style.backgroundImage = "url('https://freight.cargo.site/t/original/i/561b7e4736035c98687372ba6c41ef46377c916705ccecd0498a526089aa8634/_DSF0752-Exposure.jpg')";
+}
